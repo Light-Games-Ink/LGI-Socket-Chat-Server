cb275a53e8941772cb9b8025331db0530eb8f301
LaughingMaan <laughingmaan@icloud.com>
2015-12-13 17:05:14 +0300

    Network changes

--- LGI-Socket-Chat-Server/src/ru/lgi/main/Main.java
+++ LGI-Socket-Chat-Server/src/ru/lgi/main/Main.java
@@ -6,6 +6,12 @@ package ru.lgi.main;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+import naga.*;
 
 /**
  * @author LaughingMaan
@@ -15,12 +21,70 @@ public class Main {
 	static private AsynchronousServerSocketChannel server;
 	static String host;
 	static int port;
+	static ArrayList<NIOSocket> sockets = new ArrayList<NIOSocket>();
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
+			port = 5676;
+			NIOService service = new NIOService();
+			NIOServerSocket socket = service.openServerSocket(new InetSocketAddress("localhost",port), 10);
+			socket.listen(new ServerSocketObserverAdapter(){
+				@Override
+				public void newConnection(NIOSocket nioSocket) {
+						if(!sockets.contains(socket)){
+					    sockets.add(nioSocket);
+					    System.out.println(nioSocket.getIp()+":"+nioSocket.getPort());
+						}
+					nioSocket.listen(new SocketObserverAdapter(){
+						@Override
+						public void packetReceived(NIOSocket socket, byte[] packet) {
+							//Check the packet, then made a broadcast
+							Charset cs = Charset.forName("UTF-8");
+							String msg = new String(packet,cs);
+							System.out.println(msg);
+							for (NIOSocket socket2 : sockets) {
+								socket2.write(packet);
+							}
+							
+						}
+						@Override
+						public void connectionBroken(NIOSocket nioSocket, Exception exception)
+						{
+							System.out.println(exception.getMessage());
+							for (NIOSocket socket2 : sockets) {
+								if(socket2.equals(nioSocket)){
+									sockets.remove(socket2);
+								}
+							}
+							nioSocket.close();
+						}
+						
+					});
+				}
+				@Override
+				public void acceptFailed(IOException exception){
+					System.out.println(exception.getMessage());
+				}
+				@Override
+				public void serverSocketDied(Exception e)
+				{
+					System.out.println(e.getMessage());
+				}
+			});
+			socket.setConnectionAcceptor(ConnectionAcceptor.ALLOW);
+			while(true){
+				service.selectBlocking();
+			}
+			/*
+			NIOService service = new NIOService();
+			NIOServerSocketSSL serverSocket = service.openSSLServerSocket(null, new InetSocketAddress("localhost", 5678), 100);
+			serverSocket.setConnectionAcceptor(ConnectionAcceptor.ALLOW);
+			serverSocket.listen(null);
+			host = "localhost";
+			port = 5678;
 			server = AsynchronousServerSocketChannel.open();
 			InetSocketAddress sAddr = new InetSocketAddress(host, port);
 			server.bind(sAddr);
@@ -29,11 +93,11 @@ public class Main {
 			attach.server = server;
 			server.accept(attach, new ConnectionHandler());
 			Thread.currentThread().join();
-			
+			*/
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		//} //catch (InterruptedException e) {
+			//e.printStackTrace();
 		}
 	}
 
